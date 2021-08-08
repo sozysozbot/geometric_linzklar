32561e40fa7299f70c04241fc819a181a2ab3aab
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-08-08 21:07:34 +0900

    Refactor a bit

--- render.js
+++ render.js
@@ -36,16 +36,33 @@ const row_num = s.border_colors.length;
 
 const image_full_height = 10 + row_num * 120 + 10;
 
-const single_column = `        <${"path"} fill="#a00" d="m-10 -10h156v${image_full_height}h-156z" />\n` +
+const single_column_svg = `        <${"path"} fill="#a00" d="m-10 -10h156v${image_full_height}h-156z" />\n` +
     s.border_colors.map((color, ind) => `        <${"path"} fill="${color}" d="m0 ${120 * ind}h136v120h-136" />`).join("\n") + "\n\n" +
     Array.from({ length: row_num }, (_, ind) => `        <${"path"} fill="${s.cell_inner_color}" d="m10 ${10 + 120 * ind}h116v100h-116" />`).join("\n");
 
-const columns = Array.from(
+const columns_svg = Array.from(
     { length: column_num },
     (_, index) => `    <g id="column${column_num - 1 - index}" stroke-width="0" transform="translate(${s.viewBox_min_x + 10 + (156 + s.column_spacing) * index}, ${s.viewBox_min_y + 10})">
-${single_column}
+${single_column_svg}
     </g>`
 ).join("\n");
+
+const glyphs_svg = glyphs_to_render.filter(row => row.trim() !== "").map((row, ind) => {
+    const [initial] = [...row];
+    const rem = ind % num_of_glyphs_each_row_can_contain; // determines the y coordinate
+    const quot = Math.floor(ind / num_of_glyphs_each_row_can_contain); // determines the x coordinate
+
+    // column${column_num - 1 - index} corresponds to translate(${s.viewBox_min_x + 10 + (156 + s.column_spacing) * index}
+    // column${quot} corresponds to translate(${s.viewBox_min_x + 10 + (156 + s.column_spacing) * (column_num - quot - 1), ...}
+    const translate_x = s.viewBox_min_x + 10 + (156 + s.column_spacing) * (column_num - quot - 1);
+
+    // a clever technique from https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
+    // in which the fact that `String.prototype.split` has a second argument `.split(separator, limit)` is vrey cleverly used.
+    const vertical_pos = s.column_format.split("*", rem + 1).join("*").length;
+    const translate_y = s.viewBox_min_y + 10 + 120 * vertical_pos;
+
+    return `        <g id="${row}${(1000 + ind).toString(10).slice(1)}" transform="translate(${translate_x}, ${translate_y})">${glyph_map.get(initial)}</g>\n`
+}).join("\n");
 
 const image_full_width = column_num * (156 + s.column_spacing) - s.column_spacing;
 
@@ -53,25 +70,9 @@ const image_full_width = column_num * (156 + s.column_spacing) - s.column_spacin
 fs.writeFileSync(out_file_name,
     `<?xml version="1.0" encoding="UTF-8"?>
 <svg width="${image_full_width}px" height="${image_full_height}px" version="1.1" viewBox="${s.viewBox_min_x} ${s.viewBox_min_y} ${image_full_width} ${image_full_height}" xmlns="http://www.w3.org/2000/svg">
-${columns}
+${columns_svg}
 
     <g id="glyphs" stroke="#000" stroke-width="10" fill="none">
-${glyphs_to_render.filter(row => row.trim() !== "").map((row, ind) => {
-        const [initial] = [...row];
-        const rem = ind % num_of_glyphs_each_row_can_contain; // determines the y coordinate
-        const quot = Math.floor(ind / num_of_glyphs_each_row_can_contain); // determines the x coordinate
-
-        // column${column_num - 1 - index} corresponds to translate(${s.viewBox_min_x + 10 + (156 + s.column_spacing) * index}
-        // column${quot} corresponds to translate(${s.viewBox_min_x + 10 + (156 + s.column_spacing) * (column_num - quot - 1), ...}
-        const translate_x = s.viewBox_min_x + 10 + (156 + s.column_spacing) * (column_num - quot - 1);
-
-        // a clever technique from https://stackoverflow.com/questions/14480345/how-to-get-the-nth-occurrence-in-a-string
-        // in which the fact that `String.prototype.split` has a second argument `.split(separator, limit)` is vrey cleverly used.
-        const vertical_pos = s.column_format.split("*", rem + 1).join("*").length;
-        const translate_y = s.viewBox_min_y + 10 + 120 * vertical_pos;
-
-        return `        <g id="${row}${(1000 + ind).toString(10).slice(1)}" transform="translate(${translate_x}, ${translate_y})">${glyph_map.get(initial)}</g>\n`
-    }).join("\n")
-    }    </g>
+${glyphs_svg}    </g>
 </svg>`);
 
